0382ff6c8e92b584f6522f963b4d931b695e2f4b
ozaki <fs65ugumori@yahoo.co.jp>
2020-04-28 21:53:08 +0900

    add slack config

--- handler.ts
+++ handler.ts
@@ -1,7 +1,23 @@
+import { WebClient } from '@slack/web-api';
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 
+const { SLACK_API_TOKEN, SLACK_CHANNEL, SLACK_USERNAME } = process.env;
+
+const slackClient = new WebClient(SLACK_API_TOKEN);
+
+const pushMessage = async ({ text }) => {
+  const result = await slackClient.chat.postMessage({
+    text,
+    channel: SLACK_CHANNEL,
+    username: SLACK_USERNAME,
+    unfurl_links: true,
+  });
+  console.log({ result });
+};
+
 export const hello: APIGatewayProxyHandler = async () => {
+  pushMessage({ text: 'Hello' });
   return {
     statusCode: 200,
     body: JSON.stringify({ message: 'hello' }),
