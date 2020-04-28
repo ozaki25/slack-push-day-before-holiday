8b6883015c3161d1304985afcb8237f44f4af70e
ozaki <fs65ugumori@yahoo.co.jp>
2020-04-28 22:38:25 +0900

    add holiday logic

--- handler.ts
+++ handler.ts
@@ -1,3 +1,5 @@
+import fetch from 'node-fetch';
+import * as dayjs from 'dayjs';
 import { WebClient } from '@slack/web-api';
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
@@ -16,9 +18,49 @@ const pushMessage = async ({ text }) => {
   console.log({ result });
 };
 
+const getHolidays = async () => {
+  const res = await fetch('https://holidays-jp.github.io/api/v1/date.json');
+  return res.json();
+};
+
+const isHoliday = async (date: dayjs.Dayjs) => {
+  const formattedDate = date.format('YYYY-MM-DD');
+
+  if (date.day() === 0) {
+    console.log(`${formattedDate} is Sunday`);
+    return true;
+  }
+
+  if (date.day() === 6) {
+    console.log(`${formattedDate} is Saturday`);
+    return true;
+  }
+
+  const holidays = await getHolidays();
+
+  if (Object.keys(holidays).includes(formattedDate)) {
+    console.log(`${formattedDate} is Holiday`);
+    return true;
+  }
+
+  console.log(`${formattedDate} is Weekday`);
+  return false;
+};
+
 const message = '明日は休みだよ！残りの仕事も頑張ろう！';
 
 export const hello: APIGatewayProxyHandler = async () => {
+  const today = dayjs();
+  const tomorrow = today.add(1, 'day');
+  console.log({ today: today.toJSON(), tomorrow: tomorrow.toJSON() });
+
+  if (await isHoliday(today)) {
+    return { statusCode: 200, body: 'Today is Holiday!!!' };
+  }
+  if (!(await isHoliday(tomorrow))) {
+    return { statusCode: 200, body: "Let's work tomorrow" };
+  }
+
   pushMessage({ text: message });
   return {
     statusCode: 200,
